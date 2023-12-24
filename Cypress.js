487f6062836ba0c1c2d91c00eabf18ea033d1276
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-24 23:35:46 +0530

    Update Cypress.js

--- Cypress.js
+++ Cypress.js
@@ -1,1 +1,15 @@
+--------------------
+Official Docs Site
+--------------------
+https://docs.cypress.io/guides/getting-started/installing-cypress
+
+
+
+
+mkdir folder-name
+ls && cd folder-name
+npm install cypress --save-dev
+npx cypress open
+
+Cypress browser opens
 
