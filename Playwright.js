f3a815f5ba52bcc3f51dd2eaa90a58c3b411d55c
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-24 23:27:26 +0530

    Update Playwright.js

--- Playwright.js
+++ Playwright.js
@@ -1,1 +1,17 @@
+---------------------
+Official Docs Page
+---------------------
+https://playwright.dev/docs/intro
+
+https://npmtrends.com/cypress-vs-playwright - Cypress vs Playwright
+
+
+------------------------
+Installation Via terminal
+--------------------------
+npm init playwright@latest
+
+Js , Playwright Browsers
+
+open the code in vs code & run "npx playwright test"
 
